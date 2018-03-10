e7020c0123c5dbdc0bdaba8368eb2162429eb8dd
Brennan Heisler <learn@heislercreative.com>
2018-03-10 01:20:37 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -17,4 +17,6 @@ class Pokemon
     found_pokemon = db.execute("SELECT * FROM Pokemon WHERE id = ?", id_number).flatten
     Pokemon.new(id: found_pokemon[0], name: found_pokemon[1], type: found_pokemon[2], hp: found_pokemon[3], db: db)
   end
+  
+  
 end
